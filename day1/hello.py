635e7465d5de2971751cc855481fd8f094a720f6
Asabeneh <asabeneh@gmail.com>
2021-08-04 00:12:23 +0300

    some scripts added to the python file

--- day1/hello.py
+++ day1/hello.py
@@ -1,2 +1,11 @@
+
+first_name = 'Michael'
+last_name = 'Wossenu'
+country = 'USA'
+
 print ("python is awsome")
 print (99 + 1)
+
+# this is a python comment
+full_name = first_name + ' ' + last_name
+print(full_name, country)
